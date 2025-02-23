e6d023108c9cf81b25591fa42a658d363a2c707e
Alisher Saideshov <90091472+vishelpodishat@users.noreply.github.com>
2025-02-23 21:53:34 +0500

    fix

--- ios/Classes/GallerySaverPlugin.m
+++ ios/Classes/GallerySaverPlugin.m
@@ -1,14 +1,13 @@
-#import "GallerySaverPlugin.h"
+// #import "GallerySaverPlugin.h"
 
-#if __has_include(<gallery_saver_plus/gallery_saver_plus-Swift.h>)
-#import <gallery_saver_plus/gallery_saver_plus-Swift.h>
-#else
-#import "gallery_saver_plus-Swift.h"
-#endif
+// #if __has_include(<gallery_saver_plus/gallery_saver_plus-Swift.h>)
+// #import <gallery_saver_plus/gallery_saver_plus-Swift.h>
+// #else
+// #import "gallery_saver_plus-Swift.h"
+// #endif
 
-
-@implementation GallerySaverPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftGallerySaverPlugin registerWithRegistrar:registrar];
-}
-@end
+// @implementation GallerySaverPlugin
+// + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+//   [SwiftGallerySaverPlugin registerWithRegistrar:registrar];
+// }
+// @end
